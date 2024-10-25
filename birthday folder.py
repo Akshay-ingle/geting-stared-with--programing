a621420acc1916ce759fda98abd6bc97a55b7aa2
Akshay-ingle <akshy.nilesh.ingle@gmail.com>
2024-10-25 19:51:36 +0530

    activity homework

---
+++ birthday folder.py
@@ -0,0 +1,13 @@
+# Storing birthday dates
+birthday1 = "2000-01-15"  # Example: January 15, 2000
+birthday2 = "1995-07-23"  # Example: July 23, 1995
+birthday3 = "1988-05-10"  # Example: May 10, 1988
+birthday4 = "2002-11-30"  # Example: November 30, 2002
+birthday5 = "1999-03-05"  # Example: March 5, 1999
+
+# Printing the birthdays
+print("Birthday 1:", birthday1)
+print("Birthday 2:", birthday2)
+print("Birthday 3:", birthday3)
+print("Birthday 4:", birthday4)
+print("Birthday 5:", birthday5)
